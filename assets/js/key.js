6b0cab44a5dcab1f34ee5b44780283838e58d8d7
phillip chung <phillip.chung0@gmail.com>
2016-08-18 19:07:25 -0700

    replaced arrow.src

--- assets/js/key.js
+++ assets/js/key.js
@@ -9,7 +9,8 @@ Key.CANVAS_POS = [0,215,445,660];
 var progress = 0;
 
 Key.prototype.draw = function(ctx){
-  var arrow = document.getElementById("arrow");
+  var arrow = new Image();
+  arrow.src = "/assets/pictures/arrow_buttons.png"
   ctx.drawImage(arrow,Key.CANVAS_POS[this.idx],0,210,210,this.pos[0],this.pos[1],100,100);
 }
 
