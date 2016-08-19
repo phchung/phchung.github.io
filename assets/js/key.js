439e7b9b97fe7cba9b80534be49668033cd545fa
phillip chung <phillip.chung0@gmail.com>
2016-08-18 19:15:23 -0700

    test

--- assets/js/key.js
+++ assets/js/key.js
@@ -11,7 +11,7 @@ var progress = 0;
 Key.prototype.draw = function(ctx){
   var arrow = new Image();
   // arrow.src = "/assets/pictures/arrow_buttons.png"
-  arrow.src = "/assets/pictures/bg.png"
+  arrow.src = "/assets/pictures/arrow_buttons.png"
   ctx.drawImage(arrow,Key.CANVAS_POS[this.idx],0,210,210,this.pos[0],this.pos[1],100,100);
 }
 
